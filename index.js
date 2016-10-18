3f6545e06fe16d2bf45d7279848a6e7e714eb0e4
franexposito <8macau8@gmail.com>
2016-10-18 18:53:20 +0200

    history

--- index.js
+++ index.js
@@ -202,6 +202,54 @@ bot.onText(/\/topuser/, function(msg, match) {
   }
 });
 
+//Show history
+bot.onText(/\/history\s*(\d*)/, function(msg, match) {
+  var idGroup = msg.chat.id;
+  var num = parseInt(match[1]);
+
+  if (num < 0) {
+    num = 10;
+  } else if (num > 20) {
+    num = 20;
+  }
+
+  if (msg.chat.type !== "group" && msg.chat.type !== "supergroup") {
+    bot.sendMessage(idGroup, "This bot only works in groups");
+  } else {
+    Groups.getGroup(idGroup).then( function(data) {
+      if (data.length > 0) {
+        return data[0];
+      } else {
+        throw ({name: "NullGroupException", message: "There is no group in db"});
+      }
+    }).then( function (data) {
+      var users = data.history;
+      if (users.length > 0) {
+        users.sort(function(a, b) { return b.date - a.date; });
+        var users_text = '';
+        for (var i = 0; i < num && i < users.length; i++) {
+          var v = "--";
+          if (users[i].vote > 0)
+            v = "++";
+
+          users_text += users[i].date + ': @' + users[i].from + '-> @'+users[i].from +' '+v+'\n';
+        }
+        var final = 'Last '+num+' votes\n' + users_text;
+        bot.sendMessage(idGroup, final,  { parse_mode: "HTML" } );
+      } else {
+        bot.sendMessage(idGroup, "Nobody has votes in this group!");
+      }
+    }).catch(function (err) {
+      console.log("EROR (" + new Date() + "): " + err.message);
+      if (err.name === "NullGroupException")
+        bot.sendMessage(idGroup, "You need to start this group before. (/start@bestuserbot)");
+      else
+        bot.sendMessage(idGroup, "An error has occurred");
+    });
+  }
+});
+
+
 bot.onText(/\/help/, function(msg, match) {
   var idGroup = msg.chat.id;
   var mensaje = "Karma Bot allows you to keep the control of the user's reputation in a group.\n";
